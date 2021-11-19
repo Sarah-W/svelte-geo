fa984617266fe4a9d78c386337e6d9d1b9060cff
Sarah-W <sarah.wheaton@gmail.com>
2021-11-19 16:28:20 +1300

    docs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,8 +11,8 @@
   // import 'prismjs/themes/prism-okaidia.css'
 
 	let selection2, selection3;
-	let n = 2;
-	let nIsInfinite = false;
+	let selectModeValue = 2;
+	let selectModeValueIsInfinite = false;
 	let clicked;
 	let ordinalScale = scaleOrdinal().range([
 		'lime',
@@ -42,14 +42,16 @@
     <h3>Minimum viable</h3>
     <p>The map inherits the size of the container you put it in. 
     The bounding boxes for the layers are calculated and the projection is generated to fit automatically.</p>
-      <div class = row>
+    <div class = row>
+      <div class = output>
         <div class="basemap" >
           <BaseMap>
             <FeatureLayer geojson={coastline} />
           </BaseMap>
         </div>
-        <div class = code>
-            <SSRPrism language="svelte">
+      </div>
+      <div class = code>
+        <SSRPrism language="svelte">
               {`<script>
   import { BaseMap, FeatureLayer } from 'svelte-geo';             
   //you can use any geojson. Here we are importing it from a file. 
@@ -64,35 +66,38 @@
 
 <style> 
   .basemap {
-    width:400;
-    height:400
+    border: black solid thin;
+    background-color:#f9f9ff;
+    width: 400px;
+    height: 400px;
+    margin: 20px;
   }
-${"<"}/style>`}
-            </SSRPrism>
-        </div>
+<\/style>`}
+        </SSRPrism>
       </div>
     </div>
-
-<div class = example>
-  <h3>Projection</h3>
-
-  <p> The default projection is geoMercator, but you can use any <a href = "https://github.com/d3/d3-geo">d3-geo</a> projection function that works for your geojson.</p>
-  <p> You may switch between projections on the fly. </p>
-
-  <div class = row> 
-    <div>
-      <input type="radio" id="geoEqualEarth" name="projection" on:click={()=>projection = geoEqualEarth} checked>
-      <label for="geoEqualEarth">geoEqualEarth</label> 
-      <input type="radio" id="geoMercator" name="projection" on:click={()=>projection = geoMercator} >
-      <label for="geoMercator">geoMercator</label>
-    <div class="basemap">
-      <BaseMap projection = { projection } >
-        <FeatureLayer geojson={coastline} />
-      </BaseMap>
-    </div>
   </div>
-    <div class = code>
-       <SSRPrism language="svelte">
+
+  <div class = example>
+    <h3>Projection</h3>
+
+    <p> The default projection is geoMercator, but you can use any <a href = "https://github.com/d3/d3-geo">d3-geo</a> projection function that works for your geojson.</p>
+    <p> You may switch between projections on the fly. </p>
+
+    <div class = row> 
+      <div class = output>
+        <input type="radio" id="geoEqualEarth" name="projection" on:click={()=>projection = geoEqualEarth} checked>
+        <label for="geoEqualEarth">geoEqualEarth</label> 
+        <input type="radio" id="geoMercator" name="projection" on:click={()=>projection = geoMercator} >
+        <label for="geoMercator">geoMercator</label>
+        <div class="basemap">
+          <BaseMap projection = { projection } >
+            <FeatureLayer geojson={coastline} />
+          </BaseMap>
+        </div>  
+      </div>
+      <div class = code>
+        <SSRPrism language="svelte">
           {`<script>
   import { BaseMap, FeatureLayer } from 'svelte-geo';       
   import { geoMercator, geoEqualEarth } from 'd3-geo';
@@ -113,92 +118,102 @@ ${"<"}/style>`}
   </BaseMap>
 <div>
 
-<style> 
+  <style> 
   .basemap {
-    width:400;
-    height:400
+    border: black solid thin;
+    background-color: #f9f9ff;
+    width: 400px;
+    height: 400px;
+    margin: 20px;
   }
-${"<"}/style>`}
-      </SSRPrism>
-    </div>
-  </div> 
-</div>
-<div class = example>
-  <h3>Styling areas</h3>
-
-  <p>
-    Apply styles to areas based on feature information by supplying a styleAccessor function. The function takes
-    a feature and it's selection status, and returns a style object.
-  </p>
-  <p>
-    Areas are styled using a d3-scale linear scale, to change the fill-opacity acccording to the
-    length of their names (feature.properties.RTO2017__1). This is one method of creating a choropleth.
-  </p>
-  <div class = row>
-    <div class="basemap">
-      <BaseMap>
-        <FeatureLayer
-          geojson={rto}
-          styleAccessor={(feature) => ({
-            fill: 'tomato',
-            'fill-opacity': linearScale(feature.properties.RTO2017__1.length),
-            stroke: 'tomato',
-            'vector-effect': 'non-scaling-stroke'
-          })}
-        />
-      </BaseMap>
-    </div>
-    <div class = code>
-        <SSRPrism language="svelte">
-          {`<script>
-  import { BaseMap, FeatureLayer } from 'svelte-geo';
-  import rto from './_geojson/rto2017_simplified_3dp.geojson.json';
-  import { scaleLinear } from 'd3-scale';
-  let linearScale = scaleLinear().domain([5, 15]);
-</script>  
-<div class="basemap">
-  <BaseMap>
-    <FeatureLayer
-      geojson={rto}
-      styleAccessor={(feature) => ({
-        'fill': 'tomato',
-        'fill-opacity': linearScale(feature.properties.RTO2017__1.length),
-        'stroke': 'tomato',
-        'vector-effect': 'non-scaling-stroke'
-      })}
-    />
-  </BaseMap>
-</div>  
-
-<style> 
-  .basemap {
-    width:400;
-    height:400
-  }
-${"<"}/style>`}
+<\/style>`}
         </SSRPrism>
-    </div>
+      </div>
+    </div> 
   </div>
+  <div class = example>
+    <h3>Styling areas</h3>
 
-  <p>
-    Areas are styled using a d3-scale ordinal scale, acccording to the first letter of their name.
-  </p>
+    <p>
+      Apply styles to areas based on feature information by supplying a styleAccessor function. The function takes
+      a feature and it's selection status, and returns a style object.
+    </p>
+    <p>
+      Areas are styled using a d3-scale linear scale, to change the fill-opacity acccording to the
+      length of their names (feature.properties.RTO2017__1). This is one method of creating a choropleth.
+    </p>
+    <div class = row>
+      <div class = output>
+        <div class="basemap">
+          <BaseMap>
+            <FeatureLayer
+              geojson={rto}
+              styleAccessor={(feature) => ({
+                fill: 'tomato',
+                'fill-opacity': linearScale(feature.properties.RTO2017__1.length),
+                stroke: 'tomato',
+                'vector-effect': 'non-scaling-stroke'
+              })}
+            />
+          </BaseMap>
+        </div>
+      </div>
+      <div class = code>
+        <SSRPrism language="svelte">
+            {`<script>
+    import { BaseMap, FeatureLayer } from 'svelte-geo';
+    import rto from './_geojson/rto2017_simplified_3dp.geojson.json';
+    import { scaleLinear } from 'd3-scale';
+    let linearScale = scaleLinear().domain([5, 15]);
+  </script>  
+  <div class="basemap">
+    <BaseMap>
+      <FeatureLayer
+        geojson={rto}
+        styleAccessor={(feature) => ({
+          'fill': 'tomato',
+          'fill-opacity': linearScale(feature.properties.RTO2017__1.length),
+          'stroke': 'tomato',
+          'vector-effect': 'non-scaling-stroke'
+        })}
+      />
+    </BaseMap>
+  </div>  
 
-  <div class = row>
-    <div class="basemap">
-      <BaseMap>
-        <FeatureLayer
-          geojson={rto}
-          styleAccessor={(feature) => ({
-            'fill': ordinalScale(feature.properties.RTO2017__1[0]),
-            'stroke': 'olive',
-            'vector-effect': 'non-scaling-stroke'
-          })}
-        />
-      </BaseMap>
+  <style> 
+  .basemap {
+    border: black solid thin;
+    background-color: #f9f9ff;
+    width: 400px;
+    height: 400px;
+    margin: 20px;
+  }
+<\/style>`}
+        </SSRPrism>
+      </div>
     </div>
-    <div class = code>
-      <SSRPrism language="svelte">
+
+    <p>
+      Areas are styled using a d3-scale ordinal scale, acccording to the first letter of their name.
+    </p>
+
+    <div class = row>
+      <div class = output>
+        <div class="basemap">
+          <BaseMap>
+            <FeatureLayer
+              geojson={rto}
+              styleAccessor={(feature) => ({
+                'fill': ordinalScale(feature.properties.RTO2017__1[0]),
+                'stroke': 'olive',
+                'vector-effect': 'non-scaling-stroke'
+              })}
+            />
+          </BaseMap>
+        </div>
+      </div>  
+      <div class = code>
+        <SSRPrism language="svelte">
           {`<script>
   import { BaseMap, FeatureLayer } from 'svelte-geo';
   import rto from './_geojson/rto2017_simplified_3dp.geojson.json';
@@ -222,39 +237,42 @@ ${"<"}/style>`}
 
 <style> 
   .basemap {
-    width:400;
-    height:400
+    border: black solid thin;
+    background-color: #f9f9ff;
+    width: 400px;
+    height: 400px;
+    margin: 20px;
   }
-${"<"}/style>`}
-      </SSRPrism>
+<\/style>`}
+        </SSRPrism>
+      </div>
     </div>
   </div>
-</div>
 
-<div class = example>
-  <h3>Clickable areas</h3>
+  <div class = example>
+    <h3>Clickable areas</h3>
 
-  <p>Clicking on an area dispatches a click event. e.detail contains the geojson feature object.</p>
-  <div class = row>
-    <div>
-      <div class="basemap">
-        <BaseMap>
-          <FeatureLayer
-            geojson={rto}
-            on:click={(e) => (clicked = e.detail.feature)}
-          />
-        </BaseMap>
+    <p>Clicking on an area dispatches a click event. e.detail contains the geojson feature object.</p>
+    <div class = row>
+      <div class = output>
+        <div class="basemap">
+          <BaseMap>
+            <FeatureLayer
+              geojson={rto}
+              on:click={(e) => (clicked = e.detail.feature)}
+            />
+          </BaseMap>
+        </div>
+        <div>
+          {#if clicked}
+            <p>The last thing you clicked was:</p>
+            <SSRPrism language='js'>{JSON.stringify(clicked.properties,undefined,2)}</SSRPrism>
+          {:else}
+            <p>Click on the map!</p>
+          {/if}
+        </div>
       </div>
-      <div>
-        {#if clicked}
-          <p>The last thing you clicked was:</p>
-          <SSRPrism language='js'>{JSON.stringify(clicked.properties,undefined,2)}</SSRPrism>
-        {:else}
-          <p>Click on the map!</p>
-        {/if}
-      </div>
-    </div>
-    <div class = code>
+      <div class = code>
         <SSRPrism language="svelte">
           {`<script>
   import { BaseMap, FeatureLayer } from 'svelte-geo';
@@ -281,60 +299,58 @@ ${"<"}/style>`}
 
 <style> 
   .basemap {
-    width:400;
-    height:400
+    border: black solid thin;
+    background-color: #f9f9ff;
+    width: 400px;
+    height: 400px;
+    margin: 20px;
   }
-<\/style>
-`}
-      </SSRPrism>
-    </div>
-  </div>
-
-
-</div>
-
-<div class = example>
-  <h3>Selectable areas</h3>
-
-  <p>Selecting an area adds it's uid to the selection array, which you can bind to.</p>
-  <p>
-    You can set what is used as the uid via the idAccessor function. This function should take a
-    geojson feature and return a uid. It defaults to feature=>JSON.stringify(feature.properties).
-  </p>
-  <p>
-    Use a styleAccessor function to highlight the selected areas. The function should have as 
-    its first argument the geojson feature, and as the second a boolean which indicates whether 
-    it is selected or not. It should return a style object.   
-  </p>
-  <p>
-
-  </p>
-  <div class = row>
-    <div>
-      <p>Select up to 3 areas</p>
-
-      <p>Current Selection: {JSON.stringify(selection2)}</p>
-
-      <div class="basemap">
-        <BaseMap>
-          <FeatureLayer
-            styleAccessor={(feature, selected) => ({
-              'fill': 'black',
-              'stroke': 'black',
-              'vector-effect': 'non-scaling-stroke',
-              'fill-opacity': selected ? 0.5 : 0.1
-            })}
-            geojson={rto}
-            selectMode={3}
-            idAccessor={(feature) => feature.properties.RTO2017__1}
-            bind:selection={selection2}
-          />
-        </BaseMap>
+<\/style>`}
+        </SSRPrism>
       </div>
     </div>
-    <div class = code>
-      <SSRPrism>
-        {`<script>
+  </div>
+  <div class = example>
+    <h3>Selectable areas</h3>
+
+    <p>Selecting an area adds it's uid to the selection array, which you can bind to.</p>
+    <p>
+      You can set what is used as the uid via the idAccessor function. This function should take a
+      geojson feature and return a uid. It defaults to feature=>JSON.stringify(feature.properties).
+    </p>
+    <p>
+      Set selectMode to the number of   
+    </p>
+    <p>
+      Use a styleAccessor function to highlight the selected areas. The function should have as 
+      its first argument the geojson feature, and as the second a boolean which indicates whether 
+      it is selected or not. It should return a style object.   
+    </p>
+    <div class = row>
+      <div class = output>
+        <p>Select up to 3 areas</p>
+
+        <p>Current Selection: {JSON.stringify(selection2)}</p>
+
+        <div class="basemap">
+          <BaseMap>
+            <FeatureLayer
+              styleAccessor={(feature, selected) => ({
+                'fill': 'black',
+                'stroke': 'black',
+                'vector-effect': 'non-scaling-stroke',
+                'fill-opacity': selected ? 0.5 : 0.1
+              })}
+              geojson={rto}
+              selectMode={3}
+              idAccessor={(feature) => feature.properties.RTO2017__1}
+              bind:selection={selection2}
+            />
+          </BaseMap>
+        </div>
+      </div>
+      <div class = code>
+        <SSRPrism>{`<script>
   import { BaseMap, FeatureLayer } from 'svelte-geo';
   import rto from './_geojson/rto2017_simplified_3dp.geojson.json';
   let selection = [] 
@@ -361,52 +377,111 @@ ${"<"}/style>`}
                  
 <style> 
   .basemap {
-    width:400;
-    height:400
+    border: black solid thin;
+    background-color: #f9f9ff;
+    width: 400px;
+    height: 400px;
+    margin: 20px;
   }
 <\/style>`}
-      </SSRPrism>
-    </div>
-  </div>  
-<div>
+        </SSRPrism>
+      </div>
+    </div>  
+    <p>
+      You can change the number of areas you are choosing to select programmatically. Changing to a
+      smaller number discards the oldest selections first.
+    </p>
 
-<p>
-	You can change the number of areas you are choosing to select programmatically. Changing to a
-	smaller number discards the oldest selections first.
-</p>
+    <div class = row>
+      <div class = output>
+        <label>
+          Selecting up to
+          <input type="number" 
+            disabled={selectModeValueIsInfinite} 
+            bind:value={selectModeValue} 
+            min="0" max="30" />
+          areas.
+        </label>
+        <label>
+          <input type="checkbox" bind:checked={selectModeValueIsInfinite} />
+          Infinite
+        </label> <br />
+
+        <p>Current Selection: {JSON.stringify(selection3)}</p>
+
+        <div class="basemap">
+          <BaseMap>
+            <FeatureLayer
+              geojson={rto}
+              styleAccessor={(feature, selected) => ({
+                fill: 'red',
+                stroke: 'black',
+                'vector-effect': 'non-scaling-stroke',
+                'fill-opacity': selected ? 0.9 : 0.1
+              })}
+              selectMode={selectModeValueIsInfinite ? Infinity : selectModeValue}
+              idAccessor={(feature) => feature.properties.OBJECTID}
+              bind:selection={selection3}
+            />
+          </BaseMap>
+        </div>
+      </div>
+      <div class = code>
+        <SSRPrism>
+    {`<script>
+  import { BaseMap, FeatureLayer } from 'svelte-geo';
+  import rto from './_geojson/rto2017_simplified_3dp.geojson.json';
+  
+  let selectModeValue = 2
+  let selectModeValueIsInfinite = false
+  let selection = [] 
+</script>  
 
 <label>
 	Selecting up to
-	<input type="number" disabled={nIsInfinite} bind:value={n} min="0" max="30" />
+	<input type="number" 
+    disabled={selectModeValueIsInfinite} 
+    bind:value={selectModeValue} 
+    min="0"/>
 	areas.
 </label>
 <label>
 	<input type="checkbox" bind:checked={nIsInfinite} />
 	Infinite
 </label> <br />
-
-<p>Current Selection: {JSON.stringify(selection3)}</p>
-
+<p>Current Selection: {JSON.stringify(selection)}</p>   
 <div class="basemap">
-	<BaseMap>
-		<FeatureLayer
-			geojson={rto}
-			styleAccessor={(feature, selected) => ({
-				fill: 'red',
-				stroke: 'black',
-				'vector-effect': 'non-scaling-stroke',
-				'fill-opacity': selected ? 0.9 : 0.1
-			})}
-			selectMode={nIsInfinite ? Infinity : n}
+  <BaseMap>
+    <FeatureLayer
+      styleAccessor={(feature, selected) => ({
+        'fill': 'red',
+        'stroke': 'black',
+        'vector-effect': 'non-scaling-stroke',
+        'fill-opacity': selected ? 0.5 : 0.1
+      })}
+      geojson={rto}
+			selectMode={selectModeValueIsInfinite ? Infinity : selectModeValue}
 			idAccessor={(feature) => feature.properties.OBJECTID}
-			bind:selection={selection3}
-		/>
-	</BaseMap>
+      bind:selection={selection}
+    />
+  </BaseMap>
+</div>
+                     
+<style> 
+  .basemap {
+    border: black solid thin;
+    background-color: #f9f9ff;
+    width: 400px;
+    height: 400px;
+    margin: 20px;
+  }
+<\/style>`}
+        </SSRPrism>  
+      </div>
+    </div>
+  </div>
 </div>
 
-</div>
-</div>
-</div>
 
 
 <style>
@@ -419,11 +494,18 @@ ${"<"}/style>`}
   .row{
     display:flex
   }
+  div.output{
+    border: black solid thin;
+    background-color:#efeeee;
+    margin:28px 20px;
+    padding:10px 10px 10px 10px;
+  }
 	div.basemap {
 		border: black solid thin;
 		width: 400px;
 		height: 400px;
 		margin: 20px;
+    background-color:#f9f9ff;
 	}
   div.code{
     width: 800px;
